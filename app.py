9e904dd412c1236de0d3c7b2d5ff1a8f5d1430ee
joshkei <joshkei2001@gmail.com>
2017-08-18 18:02:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ from pyimei import ImeiSupport
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('./', methods=['GET', 'POST'])
 def index():    
     return render_template('index.html', imei=ImeiSupport.generateNew())
-
-@app.route('/thank')
-def thank():    
-    return render_template('thank.html')
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    #app.run(debug=True, host='0.0.0.0')
+    app.run()
